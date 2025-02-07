0d000a6d91abbf8f5c397bd70439e424b0912f27
Olivier Neve <olivier.ch.neve@gmail.com>
2025-02-07 11:45:30 +0100

    created card collection smart contract

---
+++ contracts/CardCollection.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CardCollection is ERC721URIStorage, Ownable {
+    uint256 public nextTokenId;
+    uint256 public constant MAX_OWNED_CARDS = 4;
+    mapping(address => uint256) public cooldowns;
+    mapping(uint256 => address[]) public previousOwners;
+
+    constructor() ERC721("CardCollection", "CARD") {}
+
+    function mintCard(string memory tokenURI) external {
+        require(balanceOf(msg.sender) < MAX_OWNED_CARDS, "Ownership limit reached");
+        
+        uint256 tokenId = nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+    }
+
+    function tradeCard(uint256 tokenId, address to) external {
+        require(ownerOf(tokenId) == msg.sender, "Not the owner");
+        require(block.timestamp >= cooldowns[msg.sender], "Cooldown active");
+        require(balanceOf(to) < MAX_OWNED_CARDS, "Recipient at max ownership");
+
+        cooldowns[msg.sender] = block.timestamp + 5 minutes;
+
+        previousOwners[tokenId].push(msg.sender);
+        _transfer(msg.sender, to, tokenId);
+    }
+
+    function getPreviousOwners(uint256 tokenId) external view returns (address[] memory) {
+        return previousOwners[tokenId];
+    }
+}
